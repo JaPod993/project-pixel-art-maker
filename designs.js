204ebd5ad1233a93808001643c10727199e75f21
Jan Podgorski <j.pod.12@gmail.com>
2018-04-10 19:34:21 +0200

    color function added

--- designs.js
+++ designs.js
@@ -7,6 +7,16 @@ const gridWitdh = document.querySelector('#inputWeight'); //szerokosc tabeli
 const sizePicker = document.querySelector('#sizePicker');
 const table = document.querySelector('#pixelCanvas');
 
+// colro set function
+function colorSet() {
+    table.addEventListener('click', function(e){
+        e.preventDefault();
+        if(e.target.nodeName === 'TD'){
+            e.target.style.backgroundColor = colorPicker.value;
+        }
+    });
+}
+
 // When size is submitted by the user, call makeGrid()
 function makeGrid(){
 
@@ -18,11 +28,12 @@ function makeGrid(){
         let tr = document.createElement('tr');
         fragment.appendChild(tr);
         for(let w = 0; w < gridWitdh.value; w++){
-            let td = document.createElement('td')
+            let td = document.createElement('td');
             tr.appendChild(td);
         }
     }
     table.appendChild(fragment);
+    colorSet();
 }
 
 sizePicker.addEventListener('submit',function(e){
